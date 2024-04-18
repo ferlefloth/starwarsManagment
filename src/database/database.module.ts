7f70da88d424083b6baef58df52fccf6d3a7a2d7
Fernando Lefloth <mellolefloth@gmail.com>
2024-04-18 18:32:57 -0300

    added process.env.mongouri

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/starWarsManagment'), 
+    MongooseModule.forRoot(process.env.MONGOURI), //starsWarsManagment 
 ],
 })
 export class DatabaseModule {}
