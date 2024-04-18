7f201469fe37ae6d2a49d75ddd8aaf9cf1999e08
Fernando Lefloth <mellolefloth@gmail.com>
2024-04-18 17:50:25 -0300

    added log in environment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { env } from 'process';
 
 async function bootstrap() {
+  console.log('environment: ' + process.env.PORT)
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors(); 
   app.useGlobalPipes(new ValidationPipe()) 
   await app.listen(3000);
