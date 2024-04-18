2f518680f2888f652cd91524c300858f51560db1
Fernando Lefloth <mellolefloth@gmail.com>
2024-04-18 20:45:00 -0300

    add starwars get hello

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,27 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `    STARWARS-MANAGMENT-API
-    <pre>
-                    .-.__      \ .-.  ___  __
-                |_|  '--.-.-(   \/\;;\_\.-._______.-.
-                (-)___     \ \ .-\ \;;\(   \       \ \
-                Y    '---._\_((Q)) \;;\\ .-\     __(_)
-                I           __'-' / .--.((Q))---'    \,
-                I     ___.-:    \|  |   \'-'_          \
-                A  .-'      \ .-.\   \   \ \ '--.__     '\
-                |  |____.----((Q))\   \__|--\_      \     '
-                    ( )        '-'  \_  :  \-' '--.___\
-                    Y                \  \  \       \(_)
-                    I                 \  \  \         \,
-                    I                  \  \  \          \
-                    A                   \  \  \          '\
-                    |                     \  \__|           '
-                                          \_:.  \
-                                            \ \  \
-                                              \ \  \
-                                              \_\_|                    
-    </pre>
-    `
+    return `    STARWARS-MANAGMENT-API `
   }
 }
